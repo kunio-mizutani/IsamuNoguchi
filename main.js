1b5fcaf74bdafbedfd0615596185acf6a9b13651
kunio-mizutani <84360745+kunio-mizutani@users.noreply.github.com>
2021-06-04 22:35:37 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+{
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const hbgMenu = document.querySelector('.hbg-menu');
+
+open.addEventListener('click',() => {
+ hbgMenu.classList.add('show');
+ open.classList.add('hide');
+});
+
+
+
+
+
+
+
+
+
+}
